297500ebd3bd32aae4fcb3cba1ba7045692dd291
Hazem El Sankari <hazemleb96@gmail.com>
2021-04-13 19:52:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Your code goes here
 document.addEventListener("DOMContentLoaded", function() {
   console.log("The DOM has loaded");
-  document.getElementById("text").textContent = "This is really cool";
+  document.getElementById("text").textContent = "This is really cool!";
 });
